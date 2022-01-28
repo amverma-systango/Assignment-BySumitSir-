df7c43da981d2018f029591e5b32270559ba0e5c
amverma-systango <amverma@isystango.com>
2022-01-28 14:39:23 +0530

    minor correction in variable name

--- CalculateSalesTax.py
+++ CalculateSalesTax.py
@@ -4,31 +4,19 @@ import csv
 productCatalogDf = pd.read_csv("ProductCatalog.csv")
 salesTaxDf = pd.read_csv("SalesTax.csv")
 
-with open("result.csv", "w", newline="") as f:
-    thewritter = csv.writer(f)
+with open("result.csv", "w", newline="") as csvFileObject:
+    thewritter = csv.writer(csvFileObject)
 
     thewritter.writerow(["Product-Name", "Product-CostPrice", "Product-SalesTax", "Product-SalesTaxAmount", "Product-FinalPrice", "Country"])
 
-    i = 0
-    print(type(float(salesTaxDf['SalseTaxInPercent'][i])))
+    salseTaxDfIndex = 0
 
-
-    while i < len(salesTaxDf):
-        j = 0
-        while j < len(productCatalogDf):
-            taxprice = (float(salesTaxDf['SalseTaxInPercent'][i])* float(productCatalogDf['ProductCost'][j]))/100
-            salesprice = float(productCatalogDf['ProductCost'][j])+taxprice
-            #print(f"{salesTaxDf['Country'][i]}         {salesTaxDf['SalseTaxInPercent'][i]}%           {productCatalogDf['ProductName'][j]}           {productCatalogDf['ProductCost'][j]} {salesprice} cost AT")
-            thewritter.writerow([productCatalogDf['ProductName'][j], productCatalogDf['ProductCost'][j], salesTaxDf['SalseTaxInPercent'][i], taxprice, salesprice, salesTaxDf['Country'][i]])
-            j+=1
-        i+=1
-
-
-#print(productCatalogDf)
-#print(salesTaxDf)
-
-
-# country product tax actualprice aftertaxprice
-
-# Country  SalseTaxInPercent    ProductName ProductCost
-
+    while salseTaxDfIndex < len(salesTaxDf):
+        productCatalogDfIndex = 0
+        while productCatalogDfIndex < len(productCatalogDf):
+            taxprice = (float(salesTaxDf['SalseTaxInPercent'][salseTaxDfIndex])* float(productCatalogDf['ProductCost'][productCatalogDfIndex]))/100
+            salesprice = float(productCatalogDf['ProductCost'][productCatalogDfIndex])+taxprice
+    
+            thewritter.writerow([productCatalogDf['ProductName'][productCatalogDfIndex], productCatalogDf['ProductCost'][productCatalogDfIndex], salesTaxDf['SalseTaxInPercent'][salseTaxDfIndex], taxprice, salesprice, salesTaxDf['Country'][salseTaxDfIndex]])
+            productCatalogDfIndex+=1
+        salseTaxDfIndex+=1
